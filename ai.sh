c8e737d28afec260d4bf69bc992d2060c8893af0
Ryan <ryandury@gmail.com>
2024-05-14 15:25:32 -0700

    Default to gpt-4o

--- ai.sh
+++ ai.sh
@@ -1,6 +1,7 @@
 args="$@"
 
 API_KEY="your-api-key"
+MODEL="gpt-4o"
 
 MESSAGE="Respond with the most suitable terminal command based on this query: <query>$args</query> Only return the command and nothing else. Do not wrap command in bash\nsudo"
 
@@ -8,7 +9,7 @@ RESPONSE=$(curl -s -X POST https://api.openai.com/v1/chat/completions \
 -H "Content-Type: application/json" \
 -H "Authorization: Bearer $API_KEY" \
 -d "{
-\"model\": \"gpt-3.5-turbo\",
+\"model\": \"$MODEL\",
 \"messages\": [
     {
     \"role\": \"system\",
@@ -27,7 +28,7 @@ if echo "$RESPONSE" | grep -q '"error"'; then
   exit 1
 fi
 
-COMMAND=$(echo "$RESPONSE" | awk -F: '/"content":/{gsub(/"|,/,""); print $2}')
+COMMAND=$(echo "$RESPONSE" | awk -F: '/"content":/{gsub(/"|,/,""); print $2}')open
 
 echo "I'm going to run the following command: $COMMAND"
 read -p "Do you want to continue? (Y/n) " choice
